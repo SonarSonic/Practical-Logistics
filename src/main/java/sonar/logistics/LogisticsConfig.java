123b4722d40d71c8b1f30ec1291a45717d56720c
AnarchySage <avarice@charter.net>
2016-07-05 12:31:23 -0500

    This will give players ability to change size's, chance
    (should work, 5k errors if i try and boot this local to test)

--- src/main/java/sonar/logistics/LogisticsConfig.java
+++ src/main/java/sonar/logistics/LogisticsConfig.java
@@ -8,6 +8,9 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 public class LogisticsConfig extends Logistics {
 
 	public static boolean sapphireOre;
+	public static int sapphireOreveinmin;
+	public static int sapphireOreveinmax;
+	public static int sapphireOreveinchance;
 	public static boolean displayMana;
 
 	public static void initConfiguration(FMLPreInitializationEvent event) {
@@ -18,6 +21,9 @@ public class LogisticsConfig extends Logistics {
 		Configuration config = new Configuration(new File("config/Practical-Logistics/Main-Config.cfg"));
 		config.load();
 		sapphireOre = config.getBoolean("Generate Ore", "settings", true, "Sapphire Ore");
+		sapphireOreveinmin = config.get("Minimum sapphire Ore vein size",2).getInt();
+		sapphireOreveinmax = config.get("Maximum sapphire Ore vein size",5).getInt();
+		sapphireOreveinchance = config.get("Maximum sapphire Ore vein chance",5).getInt();
 		displayMana = config.getBoolean("Mana", "settings", false, "Display Mana Percentage");
 		config.save();
 
